c3af3be7e70d10aec7d1d37ed1e711958b1232ab
SubhamPaul21 <subhampaul12@gmail.com>
2023-11-30 19:06:37 +0530

    optimizes reverse numeric pattern code

--- Patterns/reverse_numeric_pattern.py
+++ Patterns/reverse_numeric_pattern.py
@@ -6,10 +6,11 @@ n = 5
 3 4 5
 2 3 4 5
 1 2 3 4 5
+0 1 2 3 4 5
 """
 
 def reverse_numeric_pattern(n):
-    for i in range(n):
+    for i in range(n+1):
         for j in range(i+1):
             print((n - i) + j, end=" ")
         print()
