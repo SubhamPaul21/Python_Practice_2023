f08753bfcf1371cbc551246bf89f910be1879ffd
SubhamPaul21 <subhampaul12@gmail.com>
2023-11-30 19:23:35 +0530

    modifies reverse numerical pattern code

--- Patterns/reverse_numeric_pattern.py
+++ Patterns/reverse_numeric_pattern.py
@@ -9,10 +9,18 @@ n = 5
 0 1 2 3 4 5
 """
 
-def reverse_numeric_pattern(n):
+def reverse_numeric_pattern_approach1(n):
     for i in range(n+1):
         for j in range(i+1):
             print((n - i) + j, end=" ")
         print()
 
-reverse_numeric_pattern(5)
+
+def reverse_numeric_pattern_approach2(n):
+    for i in range(n + 1):
+        for j in range(i, -1, -1):
+            print(n - j, end=" ")
+        print()
+
+reverse_numeric_pattern_approach1(5)
+reverse_numeric_pattern_approach2(10)
