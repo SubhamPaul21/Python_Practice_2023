4b7297a15e17788d83f605264f1bb82628ea5963
SubhamPaul21 <subhampaul12@gmail.com>
2023-12-01 00:59:40 +0530

    adds pyramid numerical pattern code

---
+++ Patterns/pyramid_numerical_pattern.py
@@ -0,0 +1,37 @@
+"""
+Q1. Print the following pattern for the given number of rows.
+Input format :
+Integer N (Total no. of rows)
+
+Output format :
+Pattern in N lines
+
+Sample Input 1:
+5
+Sample Output 1:
+          1
+          232
+         34543
+        4567654
+       567898765
+
+Constraints :
+0 <= N <= 50
+"""
+
+
+def pyramid_numerical_pattern(n):
+    if 0 <= n <= 50:
+        for i in range(n):
+            for j in range(n-i-1):
+                print(" ", end=" ")
+            for k in range(i+1, (i+1)*2):
+                print(k, end=" ")
+            for l in range(i*2, i, -1):
+                print(l, end=" ")
+            print()
+    else:
+        print("Out of bound!")
+
+
+pyramid_numerical_pattern(5)
